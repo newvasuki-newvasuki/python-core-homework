1caa5fc3ea8eed457a7f2a286d149d16d104c61c
Кирилл Пятница <kirill.pyatnitsa@gmail.com>
2022-01-29 14:23:26 +0300

    home work done

--- ex2/__init__.py
+++ ex2/__init__.py
@@ -1,4 +1,6 @@
 from ex2 import fetcher
+from timeit import default_timer
+
 
 CALL_COUNT = 10
 
@@ -10,9 +12,20 @@ def benchmark(num):
     :param num: число итераций
     :return: функцию обёртку
     """
+	# поскольку декоратор принимает параметр
+	# придётся делать декоратор в декораторе
     def wrapper(func):
-        # put your code here
-        pass
+        def inner(*args,**kwargs):
+            cumulativeTime = 0
+            for itaration in range(num):
+                startTime = default_timer()
+                func(*args, **kwargs)
+                endTime = default_timer()
+                timeOfExecution = endTime - startTime
+                cumulativeTime += timeOfExecution
+                print(timeOfExecution)
+            print(cumulativeTime)
+        return inner        
     return wrapper
 
 
